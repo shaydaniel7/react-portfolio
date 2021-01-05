232196d59d8df6fb23586a87fd12c650ea8680f3
shay_daniel <shay@isom.org>
2021-01-04 18:25:18 -0800

    updated buttons on port page, also updated port background

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -29,8 +29,8 @@ function Projects() {
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src={planner} />
                         <Card.Body>
-                            <Card.Title><a href="https://shaydaniel7.github.io/day-planner/">Day Planner</a></Card.Title>
-                            <Card.Text><Button href="https://github.com/shaydaniel7/day-planner" variant="secondary">repo</Button>
+                            <Card.Title><a href="https://shaydaniel7.github.io/day-planner/">Day Planner</a><Button href="https://github.com/shaydaniel7/day-planner" variant="secondary">repo</Button></Card.Title>
+                            <Card.Text>
                             This app helps organize a day's events. It allows a person to save daily plans based on the hours of the day. The colors of the boxes change based on if the event is present, past, or upcoming.
                             </Card.Text>
                             <p className="tech"><i>Uses Moment.js, HTML, CSS, JavaScript, jQuery</i></p>
@@ -40,8 +40,8 @@ function Projects() {
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src={teamshot} />
                         <Card.Body>
-                            <Card.Title><a href="https://github.com/shaydaniel7/team.profile.generator">Team Profile Generator</a></Card.Title>
-                            <Card.Text><Button href="https://github.com/shaydaniel7/team-profile-generator" variant="secondary">repo</Button>
+                            <Card.Title><a href="https://github.com/shaydaniel7/team.profile.generator">Team Profile Generator</a><Button href="https://github.com/shaydaniel7/team-profile-generator" variant="secondary">repo</Button></Card.Title>
+                            <Card.Text>
                             This is a node command line input app in which a user can store information about employees; it then generates an HTML webpage that displays summaries for each person.
                             </Card.Text>
                             <p className="tech"><i>Uses HTML, CSS, Javascript, Node</i></p>
@@ -51,8 +51,8 @@ function Projects() {
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src={weatherapp} />
                         <Card.Body>
-                            <Card.Title><a href="https://shaydaniel7.github.io/weather-app/">Weather App</a></Card.Title>
-                            <Card.Text><Button href="https://github.com/shaydaniel7/weather-app" variant="secondary">repo</Button>
+                            <Card.Title><a href="https://shaydaniel7.github.io/weather-app/">Weather App</a><Button href="https://github.com/shaydaniel7/weather-app" variant="secondary">repo</Button></Card.Title>
+                            <Card.Text>
                             This app enables a user to search for any city and get its main weather components for the current time and date, shown at the top of the page. For each city, the app also shows its five day forecast.
                             </Card.Text>
                             <p className="tech"><i>Uses Moment.js, HTML, CSS, JavaScript, jQuery</i></p>
@@ -69,8 +69,8 @@ function Projects() {
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src={burgershot} />
                         <Card.Body>
-                            <Card.Title><a href="https://warm-woodland-61603.herokuapp.com/">Burger Logger</a></Card.Title>
-                            <Card.Text><Button href="https://github.com/shaydaniel7/burger-logger" variant="secondary">repo</Button>
+                            <Card.Title><a href="https://warm-woodland-61603.herokuapp.com/">Burger Logger</a><Button href="https://github.com/shaydaniel7/burger-logger" variant="secondary">repo</Button></Card.Title>
+                            <Card.Text>
                             This app is a logger that uses MySQL, Node, Express, Handlebars and an ORM. It follows the MVC design pattern and uses Node and MySQL to query and route data, and Handlebars to generate the HTML.
                             </Card.Text>
                             <p className="tech"><i>Uses: HTML, CSS, Javascript, Node</i></p>
@@ -80,8 +80,8 @@ function Projects() {
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src={noteshot} />
                         <Card.Body>
-                            <Card.Title><a href="https://stormy-garden-29411.herokuapp.com/">Note Taker</a></Card.Title>
-                            <Card.Text><Button href="https://github.com/shaydaniel7/notetaker" variant="secondary">repo</Button>
+                            <Card.Title><a href="https://stormy-garden-29411.herokuapp.com/">Note Taker</a><Button href="https://github.com/shaydaniel7/notetaker" variant="secondary">repo</Button></Card.Title>
+                            <Card.Text>
                             This app allows a user to create and save notes, view previously saved notes, and delete previously saved notes. Being able to take persistent notes allows users to have written information available when needed.
                             </Card.Text>
                             <p className="tech"><i>Uses HTML, CSS, JavaScript, Node.js, Express</i></p>
@@ -91,8 +91,8 @@ function Projects() {
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src={mainpage} />
                         <Card.Body>
-                            <Card.Title><a href="https://shaydaniel7.github.io/dinner-and-a-movie">Dinner and a Movie</a></Card.Title>
-                            <Card.Text><Button href="https://github.com/shaydaniel7/dinner-and-a-movie" variant="secondary">repo</Button>
+                            <Card.Title><a href="https://shaydaniel7.github.io/dinner-and-a-movie">Dinner and a Movie</a> <Button href="https://github.com/shaydaniel7/dinner-and-a-movie" variant="secondary">repo</Button> </Card.Title>
+                            <Card.Text>
                             This application provides some guidance on the simple yet difficult task of picking where to eat and what movie to watch. There are two sets of input fields for the user to fill out in order to retrieve results.
                             </Card.Text>
                             <p className="tech"><i>Uses HTML, CSS, JavaScript, jQuery</i></p>
@@ -109,8 +109,8 @@ function Projects() {
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src={snareshot3} />
                         <Card.Body>
-                            <Card.Title><a href="http://w14project2.herokuapp.com/">Snare Music Hosting</a></Card.Title>
-                            <Card.Text><Button href="https://github.com/Tbonexas/project2" variant="secondary">repo</Button>
+                            <Card.Title><a href="http://w14project2.herokuapp.com/">Snare Music Hosting</a><Button href="https://github.com/Tbonexas/project2" variant="secondary">repo</Button></Card.Title>
+                            <Card.Text>
                             This app helps connects property owner with musicians who need a place to play, practice and record.  Future developments include connecting musicians with other musicians.
                             </Card.Text>
                             <br />
@@ -122,8 +122,10 @@ function Projects() {
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src={directory} />
                         <Card.Body>
-                            <Card.Title><a href="https://shaydaniel7.github.io/user-directory/">Employee Directory</a></Card.Title>
-                            <Card.Text><Button href="https://github.com/shaydaniel7/user-directory" variant="secondary">repo</Button>
+                            <Card.Title><a href="https://shaydaniel7.github.io/user-directory/">Employee Directory</a>
+                            <Button href="https://github.com/shaydaniel7/user-directory" variant="secondary">repo</Button>
+                            </Card.Title>
+                            <Card.Text>
                             This React app is an Employee Directory that an employer can use to view their entire staff at once, have quick access to their profiles, sort by first or last name, as well as search for a particular person.
                             <br /><br />
                             </Card.Text>
@@ -135,8 +137,9 @@ function Projects() {
                     <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src={budget} />
                         <Card.Body>
-                            <Card.Title><a href="https://intense-spire-96483.herokuapp.com/">Progressive Budget</a></Card.Title>
-                            <Card.Text><Button href="https://github.com/shaydaniel7/progressive-budget" variant="secondary">repo</Button>
+                            <Card.Title><a href="https://intense-spire-96483.herokuapp.com/">Progressive Budget</a>
+                            <Button href="https://github.com/shaydaniel7/progressive-budget" variant="secondary">repo</Button></Card.Title>
+                            <Card.Text>
                             This app enables a user to search for any city and get its main weather components for the current time and date, shown at the top of the page. For each city, the app also shows its 5-day forecast.
                             </Card.Text>
                             <p className="tech"><i>Uses HTML, CSS, JavaScript, Node.js, Express, MongoDB, MongoDB Atlas</i></p>
